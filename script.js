7dc7ff8fb5b6ef03ee12e7b698c743ecbdd208e8
jane <janeagadiabiz@gmail.com>
2022-10-04 00:54:59 -1100

    changes

--- script.js
+++ script.js
@@ -7,16 +7,20 @@ const btnsTabsPair = document.querySelectorAll(".btns-tab");
 const serviceContent = document.querySelectorAll(".service-content");
 
 btnTabs.addEventListener("click", function (e) {
-  const clicked = e.target;
+  const clicked = e.target.closest(".btns-tab");
   // console.log(clicked);
 
+  if (!clicked) return;
+
   btnsTabsPair.forEach((btn) => btn.classList.remove("btns-tab-active"));
-  clicked.classList.add("btns-tab-active");
 
   serviceContent.forEach((content) =>
     content.classList.remove("service-content-active")
   );
+
+  clicked.classList.add("btns-tab-active");
   document
+
     .querySelector(`.service-content-${clicked.dataset.tab}`)
     .classList.add("service-content-active");
 });
